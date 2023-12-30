26a6ba7f5a8b21eaec20b24db5418cddfdefc5ff
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-12-30 23:46:44 +0300

    initial commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 
 export default function Todo(){
-    const [todo, setTodo] = useState()
+    const [todo, setTodo] = useState("") //set what is being input to todo
+    const handleChange=(event) => {
+        setTodo(event.target.value)
+    }
     return <div> 
         <form>
-            <input type="text"></input>
+            <input value={todo} onChange={handleChange} type="text"></input>
             <button type="submit"> Add </button>
         </form>
     </div>
